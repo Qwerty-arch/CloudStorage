a1a70fbd630d3221c1bd8c34a0896cff5e0f1c6f
Vlad <oshovsky.vlad@yandex.ru>
2020-10-05 01:58:58 +0800

    Commit 1 File send

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,35 @@
+
+import javax.swing.*;
+import java.nio.file.Paths;
+import java.util.concurrent.CountDownLatch;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        CountDownLatch networkStarter = new CountDownLatch(1);
+        new Thread(() -> Network.getInstance().start(networkStarter)).start();
+        networkStarter.await();
+
+        FileSender.sendFile(Paths.get("client","demo.txt"), Network.getInstance().getCurrentChannel(), future -> {
+            if (!future.isSuccess()) {
+                future.cause().printStackTrace();
+//                Network.getInstance().stop();
+            }
+            if (future.isSuccess()) {
+                System.out.println("Файл успешно передан");
+//                Network.getInstance().stop();
+            }
+        });
+//        Thread.sleep(2000);
+        FileSender.sendFile(Paths.get("client","demo1.txt"), Network.getInstance().getCurrentChannel(), future -> {
+            if (!future.isSuccess()) {
+                future.cause().printStackTrace();
+//                Network.getInstance().stop();
+            }
+            if (future.isSuccess()) {
+                System.out.println("Файл успешно передан");
+//                Network.getInstance().stop();
+            }
+        });
+    }
+}
+
---
+++ common/src/main/java/FileSender.java
@@ -0,0 +1,41 @@
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+
+public class FileSender {
+    public static void sendFile(Path path, Channel channel, ChannelFutureListener finishListener) throws IOException {
+        FileRegion region = new DefaultFileRegion(path.toFile(), 0, Files.size(path));
+        byte[] filenameBytes = path.getFileName().toString().getBytes(StandardCharsets.UTF_8);
+        // 1 + 4 +filenameBytes.length + 8 -> SIGNAL_BYTE FILENAME_LENGTH(int) + FILENAME + FILE_LENGTH(long)
+        ByteBuf buf = ByteBufAllocator.DEFAULT.directBuffer(1 + 4 +filenameBytes.length + 8);
+        buf.writeByte((byte) 25);
+        buf.writeInt(filenameBytes.length);
+        buf.writeBytes(filenameBytes);
+        buf.writeLong(Files.size(path));
+        channel.writeAndFlush(buf);
+
+        ChannelFuture transferOperationFuture = channel.writeAndFlush(region);
+        if (finishListener != null) {
+            transferOperationFuture.addListener(finishListener);
+        }
+    }
+}
